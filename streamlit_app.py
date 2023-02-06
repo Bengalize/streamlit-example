8f93e8580d0dd9ed6ed99a3ed43585cfc35f3d01
Bengalize <121535934+Bengalize@users.noreply.github.com>
2023-02-06 14:09:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,34 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
-"""
-# Welcome to Streamlit!
+@st.cache
+def load_data(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    table = soup.find("table")
+    df = pd.read_html(str(table))[0]
+    return df
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def main():
+    st.title("Football Player Analytics Dashboard")
+    
+    player = st.text_input("Enter a player name")
+    position = st.selectbox("Select a position", ["Forward", "Midfielder", "Defender", "Goalkeeper"])
+    
+    if player and position:
+        fbref_url = f"https://fbref.com/en/players/{player.lower().replace(' ', '-')}/comps"
+        markstat_url = f"https://markstats.club/player/{player.lower().replace(' ', '-')}/{position.lower()}"
+        
+        fbref_data = load_data(fbref_url)
+        markstat_data = load_data(markstat_url)
+        
+        st.write("Fbref Data")
+        st.write(fbref_data)
+        
+        st.write("Markstats Data")
+        st.write(markstat_data)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    main()
